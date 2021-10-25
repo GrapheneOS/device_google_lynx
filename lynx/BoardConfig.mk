2aa4ba2268903bb8a43ae3d3770b4f15bdb4f7f0
Cyan_Hsieh <cyanhsieh@google.com>
2021-10-25 18:10:38 +0800

    Add device lynx
    based on panther 0e0ae4dd4045e00dd50826d5c411351d47949d92
    No-Typo-Check: based on above commit
    
    Bug: 202250383
    Change-Id: I7dbcba9fc0644cf8e547c59b14a544701adc484b

---
+++ lynx/BoardConfig.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2021 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+TARGET_BOARD_INFO_FILE := device/google/lynx/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := lynx
+TARGET_SCREEN_DENSITY := 420
+BOARD_USES_GENERIC_AUDIO := true
+USES_DEVICE_GOOGLE_LYNX := true
+
+include device/google/gs201/BoardConfig-common.mk
+-include vendor/google_devices/gs201/prebuilts/BoardConfigVendor.mk
+include device/google/lynx-sepolicy/lynx-sepolicy.mk
