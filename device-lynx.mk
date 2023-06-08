c7140feb260301f9336ad339559989172ec0f27b
chenkris <chenkris@google.com>
2023-06-08 09:54:02 +0000

    Move goodix mk files from device/google/gs101 to vendor/goodix/udfps
    Bug: 270657514
    Test: build
    Change-Id: I8d9829fd521451e1260e8a6a4de92c515cbda6d0

--- device-lynx.mk
+++ device-lynx.mk
@@ -145,11 +145,11 @@ include device/google/lynx/bluetooth/qti_default.mk
 
 # Fingerprint HAL
 GOODIX_CONFIG_BUILD_VERSION := g7_trusty
-include device/google/gs101/fingerprint/udfps_common.mk
+include vendor/goodix/udfps/configuration/udfps_common.mk
 ifeq ($(filter factory%, $(TARGET_PRODUCT)),)
-include device/google/gs101/fingerprint/udfps_shipping.mk
+include vendor/goodix/udfps/configuration/udfps_shipping.mk
 else
-include device/google/gs101/fingerprint/udfps_factory.mk
+include vendor/goodix/udfps/configuration/udfps_factory.mk
 endif
 
 # Vibrator HAL
