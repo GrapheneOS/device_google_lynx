ecb5502fcdcc26a1fedd776c0b368357acf6c615
Roshan Pius <rpius@google.com>
2023-12-01 19:19:56 +0000

    Revert^2 "lynx: Replace NFC app with NFC apex"
    b1df77943111252c347eab44e1c55158384328aa
    
    Bug: 303286040
    Test: Compiles
    
    Change-Id: I9ae126ca8228c293bb7a88bed645528db3e0642b

--- device-lynx.mk
+++ device-lynx.mk
@@ -80,7 +80,7 @@ PRODUCT_COPY_FILES += \
     device/google/lynx/nfc/libnfc-nci-lynx.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += \
-	NfcNci \
+	$(RELEASE_PACKAGE_NFC_STACK) \
 	Tag \
 	android.hardware.nfc-service.st
 
