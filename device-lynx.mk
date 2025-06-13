fa0f4569ac24368d60c4ad72f23f81291c2474ec
Fred Underwood <underwoodfred@proton.me>
2025-06-14 06:39:18 +1000

    remove adevtool-handled vibrator properties

--- device-lynx.mk
+++ device-lynx.mk
@@ -130,14 +130,6 @@ include device/google/lynx/bluetooth/qti_default.mk
 $(call soong_config_set,haptics,kernel_ver,v$(subst .,_,$(TARGET_LINUX_KERNEL_VERSION)))
 ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 ACTUATOR_MODEL := legacy_zlra_actuator
-PRODUCT_VENDOR_PROPERTIES += \
-	ro.vendor.vibrator.hal.f0.comp.enabled=1 \
-	ro.vendor.vibrator.hal.redc.comp.enabled=0 \
-	persist.vendor.vibrator.hal.context.enable=false \
-	persist.vendor.vibrator.hal.context.scale=40 \
-	persist.vendor.vibrator.hal.context.fade=true \
-	persist.vendor.vibrator.hal.context.cooldowntime=1600 \
-	persist.vendor.vibrator.hal.context.settlingtime=5000
 
 # Override Output Distortion Gain
 PRODUCT_VENDOR_PROPERTIES += \
