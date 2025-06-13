01489bf8814a3285fab3ebbb2b6e5bd80e7e3f9c
Fred Underwood <underwoodfred@proton.me>
2025-06-14 06:50:31 +1000

    add TODO

--- device-lynx.mk
+++ device-lynx.mk
@@ -135,6 +135,8 @@ $(call soong_config_set,haptics,kernel_ver,v$(subst .,_,$(TARGET_LINUX_KERNEL_VE
 ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 ACTUATOR_MODEL := legacy_zlra_actuator
 
+# TODO: Pull these files from Android 16 stock factory image and move them into this repo with our
+# changes applied.
 # Location
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
     PRODUCT_COPY_FILES += \
