a78b91834bb8f3106c70e0cfc8f75f725f48b185
Fred Underwood <underwoodfred@proton.me>
2025-06-14 06:22:26 +1000

    remove adevtool-handled bluetooth properties

--- bluetooth/qti_default.mk
+++ bluetooth/qti_default.mk
@@ -14,27 +14,6 @@
 # limitations under the License.
 #
 
-# Bluetooth Services sysprop for A2DP AAC VBR and A2DP offloading
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.bluetooth.a2dp_aac.vbr_supported=true \
-	ro.bluetooth.a2dp_offload.supported=true \
-	persist.bluetooth.a2dp_offload.disabled=false \
-	persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac
-
-# Bluetooth LE Audio
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.bluetooth.leaudio_switcher.supported=false \
-	bluetooth.profile.bap.unicast.client.enabled?=false \
-	bluetooth.profile.csip.set_coordinator.enabled?=false \
-	bluetooth.profile.hap.client.enabled?=false \
-	bluetooth.profile.mcp.server.enabled?=false \
-	bluetooth.profile.ccp.server.enabled?=false \
-	bluetooth.profile.vcp.controller.enabled?=false \
-
-# Bluetotoh Auto On feature
-PRODUCT_PRODUCT_PROPERTIES += \
-	bluetooth.server.automatic_turn_on=true
-
 # Bluetooth HAL and Pixel extension
 DEVICE_MANIFEST_FILE += \
 	device/google/lynx/bluetooth/manifest_bluetooth.xml
