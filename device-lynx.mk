9a991d33a8113892714d7ce8cd7d1d2689079bce
Fred Underwood <underwoodfred@proton.me>
2025-06-14 07:28:36 +1000

    Revert "remove adevtool-handled files in conf/"
    This file is modified downstream, so can't be removed.
    
    This reverts commit a33eba77c18fbb72369c78fccd39f5fe65c4e7c9.

--- device-lynx.mk
+++ device-lynx.mk
@@ -38,6 +38,10 @@ $(call soong_config_set,lyric,camera_hardware,lynx)
 $(call soong_config_set,lyric,tuning_product,lynx)
 $(call soong_config_set,google3a_config,target_device,lynx)
 
+# Init files
+PRODUCT_COPY_FILES += \
+	device/google/lynx/conf/init.lynx.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.lynx.rc
+
 # Recovery files
 PRODUCT_COPY_FILES += \
         device/google/lynx/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.lynx.rc
