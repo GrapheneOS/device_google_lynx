5814e23c3f02b40ef91abceda7233544813333b3
Fred Underwood <underwoodfred@proton.me>
2025-06-13 14:26:42 +1000

    remove adevtool-handled property ro.odm.build.media_performance_class

--- device-lynx.mk
+++ device-lynx.mk
@@ -54,9 +54,6 @@ PRODUCT_COPY_FILES += \
 	device/google/lynx/init.insmod.lynx.cfg:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init.insmod.lynx.cfg
 endif
 
-# Media Performance Class 13
-PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=33
-
 # Display Config
 PRODUCT_COPY_FILES += \
         device/google/lynx/lynx/display_colordata_dev_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_colordata_dev_cal0.pb \
