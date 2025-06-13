dd5f3e276411fb5165268dab6ee1b8d57dfa197c
Fred Underwood <underwoodfred@proton.me>
2025-06-14 06:53:22 +1000

    remove adevtool-handled DCK properties

--- device-lynx.mk
+++ device-lynx.mk
@@ -161,11 +161,6 @@ endif
 # Wifi HAL
 PRODUCT_SOONG_NAMESPACES += hardware/qcom/wlan/wcn6740
 
-# DCK properties based on target
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.gms.dck.eligible_wcc=2 \
-    ro.gms.dck.se_capability=1
-
 # WIFI COEX
 PRODUCT_COPY_FILES += \
 	device/google/lynx/wifi/coex_table.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/coex_table.xml
